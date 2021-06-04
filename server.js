18599dd2eb51b98160c06fb4412c43c168877488
nushkymohamed <nuskymohamed1999@gmai.com>
2021-06-04 23:06:02 +0530

    Backend Create post View post done

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const HomeRouter = require('./routes/home.router');
+const PostRouter = require('./routes/post.routes');
 
+//Koa Application
 const app = new Koa();
 
+//Registering the bodyParser
+app.use(bodyParser());
+
+//Registered Routes
+app.use(HomeRouter.routes())
+    .use(HomeRouter.allowedMethods());
+
+app.use(PostRouter.routes())
+    .use(PostRouter.allowedMethods());
+
 app.use(ctx =>{
-    ctx.body = "Conference Management System Backend"
+    ctx.body = "Hello";
 })
 
 app.listen(3000);
