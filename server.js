28aedeea38b37dd65197d8e462cebee4cd6f5186
Zumry <amzumry@gmail.com>
2021-06-04 23:32:03 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const LoginRoutes = require('./routes/Login.routes');
+const RegisterRoutes = require('./routes/Register.routes');
 
 const app = new Koa();
+
+app.use(bodyParser());
+
+app.use(LoginRoutes.routes()).use(LoginRoutes.allowedMethods());
 
 app.use(ctx =>{
     ctx.body = "Conference Management System Backend"
